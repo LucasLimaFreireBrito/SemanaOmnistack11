609858f0752cfccef1b85b0fc1c48ccbe74fa452
Lucas Lima Freire Brito <lucas.lfreire@hotmail.com>
2020-03-29 19:14:26 -0300

    front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
-  const [Counter, setCounter] = useState(0);
-
-  function increment(){
-    setCounter(Counter + 1);
-    // console.log({counter});
-  }
-
   return (
-    <div>
-    <Header>Contador: {Counter}</Header>
-    <button onClick={increment}>Incremetar</button>
-    </div>
+    <Routes />
   );
 }
 
